20df35d95c571e9a764e73cd93a76bdded759cb2
Haider Ali <haider.lee23@gmail.com>
2019-02-13 02:20:04 +0530

    Project and database setup

---
+++ app/models.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+from app import db
+
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), index=True, unique=True)
+    email = db.Column(db.String(120), index=True, unique=True)
+    password_hash = db.Column(db.String(128))
+    posts = db.relationship('Post', backref='author', lazy='dynamic')
+    # one to many
+
+    def __repr__(self):
+        # tells Python how to print objects of this class
+        # useful for debugging
+        return '<User {}>'.format(self.username)
+
+
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    body = db.Column(db.String(140))
+    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    # flask-sqlalchemy uses snakecase table name by default
+
+    def __repr__(self):
+        return '<Post {}>'.format(self.body)
