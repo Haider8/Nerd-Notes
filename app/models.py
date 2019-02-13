52487b42db56fdd6d5484292596d58dfb21845b4
Haider Ali <haider.lee23@gmail.com>
2019-02-13 13:10:58 +0530

    Add login feature

--- app/models.py
+++ app/models.py
@@ -1,8 +1,11 @@
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import UserMixin
 from app import db
+from app import login
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
@@ -10,10 +13,21 @@ class User(db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     # one to many
 
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
+
     def __repr__(self):
         # tells Python how to print objects of this class
         # useful for debugging
         return '<User {}>'.format(self.username)
+
+
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
 
 
 class Post(db.Model):
