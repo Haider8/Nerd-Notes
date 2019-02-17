0f720e20afcff389fe8013758a23d2abdd6fde86
Haider Ali <haider.lee23@gmail.com>
2019-02-17 15:51:41 +0530

    Add profile page and last seen

--- app/routes.py
+++ app/routes.py
@@ -8,6 +8,7 @@ from flask import request
 from werkzeug.urls import url_parse
 from app import db
 from app.forms import RegistrationForm
+from datetime import datetime
 
 
 @app.route('/register', methods=['GET', 'POST'])
@@ -43,6 +44,20 @@ def index():
     return render_template('index.html', title='Home', posts=posts)
 
 
+@app.before_request
+def before_request():
+    # The @before_request decorator from Flask register
+    # the decorated function to be executed right before
+    # the view function (every view function)
+    if current_user.is_authenticated:
+        # when current_user is referenced, Flask-Login
+        # will invoke the user loader callback function,
+        # which will run a database query that will put
+        # the target user in the database session
+        current_user.last_seen = datetime.utcnow()
+        db.session.commit()
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if current_user.is_authenticated:
@@ -68,3 +83,14 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/user/<username>')
+@login_required
+def user(username):
+    user = User.query.filter_by(username=username).first_or_404()
+    posts = [
+        {'author': user, 'body': 'Test post #1'},
+        {'author': user, 'body': 'Test post #2'}
+    ]
+    return render_template('user.html', user=user, posts=posts)
