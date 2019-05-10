78aebe947cd1edbf03b1ec9155b7384bc4f4184b
Haider Ali <haider.lee23@gmail.com>
2019-05-10 14:23:46 +0530

    enh: Send Email Asynchronously

--- app/email.py
+++ app/email.py
@@ -1,13 +1,23 @@
 from flask_mail import Message
 from app import mail, app
 from flask import render_template
+from threading import Thread
+
+
+def send_async_email(app, msg):
+    # This function will run in the background thread.
+    # When the process completes the thread will end
+    # and clean itself up.
+    with app.app_context():
+        mail.send(msg)
 
 
 def send_email(subject, sender, recipients, text_body, html_body):
     msg = Message(subject, sender=sender, recipients=recipients)
     msg.body = text_body
     msg.html = html_body
-    mail.send(msg)
+    # mail.send(msg)
+    Thread(target=send_async_email, args=(app, msg)).start()
 
 
 def send_password_reset_email(user):
