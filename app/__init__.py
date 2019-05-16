c836466d39e31500338f287f0ddffa7914cfc6ea
Haider Ali <haider.lee23@gmail.com>
2019-05-16 16:12:51 +0530

    factory function for instance init.

--- app/__init__.py
+++ app/__init__.py
@@ -7,51 +7,69 @@ import logging
 from logging.handlers import SMTPHandler
 from flask_mail import Mail
 
-app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
-mail = Mail(app)
-migrate = Migrate(app, db)
-login = LoginManager(app)
+
+# Create instances with no argument. Will be
+# initialized in the function.
+db = SQLAlchemy()
+migrate = Migrate()
+login = LoginManager()
+mail = Mail()
 login.login_view = 'auth.login'  # login function
 
-# from app import routes, models, errors
 
-from app.errors import bp as errors_bp
-app.register_blueprint(errors_bp)
+def create_app(config_class=Config):
+    # constructs a Flask application instance
+    # and eliminate the global variable.
+    # Application factory function.
 
-from app.auth import bp as auth_bp
-app.register_blueprint(auth_bp, url_prefix='/auth')
+    app = Flask(__name__)
+    app.config.from_object(config_class)
 
-from app.main import bp as main_bp
-app.register_blueprint(main_bp)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    login.init_app(app)
+    mail.init_app(app)
 
+    from app.errors import bp as errors_bp
+    app.register_blueprint(errors_bp)
 
-if not app.debug:
-    # enable the email logger when the
-    # application is running without debug
-    # also when email server exists in the
-    # configuration file
-    if app.config['MAIL_SERVER']:
-        auth = None
-        if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
-            auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
-        secure = None
-        if app.config['MAIL_USE_TLS']:
-            secure = ()
-        mail_handler = SMTPHandler(
-            mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
-            fromaddr='no-reply@' + app.config['MAIL_SERVER'],
-            toaddrs=app.config['ADMINS'], subject='Microblog Failure',
-            credentials=auth, secure=secure)
-        mail_handler.setLevel(logging.ERROR)
-        app.logger.addHandler(mail_handler)
+    from app.auth import bp as auth_bp
+    app.register_blueprint(auth_bp, url_prefix='/auth')
 
-    # for heroku logs
-    if app.config['LOG_TO_STDOUT']:
-        stream_handler = logging.StreamHandler()
-        stream_handler.setLevel(logging.INFO)
-        app.logger.addHandler(stream_handler)
+    from app.main import bp as main_bp
+    app.register_blueprint(main_bp)
+
+    # The app.testing flag is going to be True when running
+    # unit tests, due to the TESTING variable being set to
+    # True in the configuration.
+    if not app.debug and not app.testing:
+        # enable the email logger when the
+        # application is running without debug
+        # also when email server exists in the
+        # configuration file. All this logging
+        # is skipped during unit tests.
+        if app.config['MAIL_SERVER']:
+            auth = None
+            if app.config['MAIL_USERNAME'] or app.config['MAIL_PASSWORD']:
+                auth = (app.config['MAIL_USERNAME'], app.config['MAIL_PASSWORD'])
+            secure = None
+            if app.config['MAIL_USE_TLS']:
+                secure = ()
+            mail_handler = SMTPHandler(
+                mailhost=(app.config['MAIL_SERVER'], app.config['MAIL_PORT']),
+                fromaddr='no-reply@' + app.config['MAIL_SERVER'],
+                toaddrs=app.config['ADMINS'], subject='Microblog Failure',
+                credentials=auth, secure=secure)
+            mail_handler.setLevel(logging.ERROR)
+            app.logger.addHandler(mail_handler)
+
+        # for heroku logs
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+
+    return app
 
 
 from app import models
